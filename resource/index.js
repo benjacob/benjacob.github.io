f67067ae1720edd465fb2f777f739ae0673fe9e8
Ben Jacob <mail@benjacob.in>
2018-10-12 13:25:04 +0530

    landingpage
    minimum info

--- resource/index.js
+++ resource/index.js
@@ -2,6 +2,8 @@ var pre = document.getElementsByTagName('pre')[0];
 var txt = pre.innerHTML;
 txt = txt.replace('@benjacob', '<a target="_blank" href="https://twitter.com/intent/follow?original_referer=https%3A%2F%2Fabout.twitter.com%2Fresources%2Fbuttons&screen_name=benjacob&tw_p=followbutton&variant=2.0">@benjacob</a>');
 txt = txt.replace('in/benjacob', '<a target="_blank" href="https://www.linkedin.com/in/benjacob">in/benjacob</a>');
+txt = txt.replace('-benjacob', '<a target="_blank" href="http://instagram.com/benjacob/">@benjacob</a>');
+
 txt = txt.replace('<mailto>', '<a href="mailto:hello@benjacob.in?Subject=Website%20enquiry" target="_top">hello@benjacob.in</a>');
 
 txt = txt.replace('-x-', 'mail');
